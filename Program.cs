07a3a021478e742c36077a0c2434173c0541eafe
Ana Júlia Medeiros <julinhamedeiros2013@gmail.com>
2024-01-12 19:02:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PrimeiroProjeto
+{
+
+    public class MeuPrimeiroPrograma
+    {
+
+        // método Main - Método Principal
+
+        public static void Main(string[] args)
+        {
+
+            // seu código aqui
+
+            // testando uma impressão na tela
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
